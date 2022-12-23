3379dddfa23138992e6f6a84e4504f7c02498df2
jero <jeroascode@gmail.com>
2022-12-23 16:16:33 -0500

    MVP

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./styled.css"
 
 const CardProducts = ({ product }) => {
   return (
-    <div>
+    <div className="container-card-product">
       <h2>{product.name}</h2>
       <label>{product.available}</label>
     </div>
